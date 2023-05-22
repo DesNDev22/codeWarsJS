403bc7c54ed8a9d33d3602af6e042ccbcc836a4c
Luis Carrera <=>
2023-05-22 14:40:18 -0500

    pushing some more katas

--- class29-32b.js
+++ class29-32b.js
@@ -193,3 +193,18 @@ console.log(solution('world'), 'dlrow');
 console.log(solution('hello'), 'olleh');
 console.log(solution(''), '');
 console.log(solution('h'), 'h');
+
+//
+
+function descendingOrder(n){
+  //...
+  return Number(n.toString().split('').sort((a, b) => b - a).join(''))
+}
+
+console.log(descendingOrder(0), 0)
+console.log(descendingOrder(1), 1)
+console.log(descendingOrder(111), 111)
+console.log(descendingOrder(15), 51)
+console.log(descendingOrder(1021), 2110)
+console.log(descendingOrder(123456789), 987654321)
+
