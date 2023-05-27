20cc48a1a984f4e55a4e44d25a0e5a680f574e47
Luis Carrera <luiscarreradesndev@gmail.com>
2023-05-27 16:38:11 -0500

    new katas added

--- class29-32c.js
+++ class29-32c.js
@@ -127,3 +127,37 @@ function expressionMatter(a, b, c) {
   console.log("String.prototype.toAlternatingCase".toAlternatingCase(), "sTRING.PROTOTYPE.TOaLTERNATINGcASE");
   console.log("Hello World".toAlternatingCase().toAlternatingCase(), "Hello World");
   
+  //
+
+  function litres(time) {
+    return Math.floor(time * .5)
+  }
+  
+  console.log(litres(2), 1, 'should return 1 litre');
+  console.log(litres(1.4), 0, 'should return 0 litres');
+  console.log(litres(12.3), 6, 'should return 6 litres');
+  console.log(litres(0.82), 0, 'should return 0 litres');
+  console.log(litres(11.8), 5, 'should return 5 litres');
+  console.log(litres(1787), 893, 'should return 893 litres');
+  console.log(litres(0), 0, 'should return 0 litres');
+
+  //
+
+  function sumStr(a,b) {
+    return (+a + +b).toString()
+  }
+  
+  console.log(sumStr("4","5"), "9");
+  console.log(sumStr("34","5"), "39");
+
+  //
+
+  function otherAngle(a, b) {
+    return 180 - (a + b);
+  }
+  
+  console.log(otherAngle(30, 60), 90);
+  console.log(otherAngle(60, 60), 60);
+  console.log(otherAngle(43, 78), 59);
+  console.log(otherAngle(10, 20), 150);
+  
