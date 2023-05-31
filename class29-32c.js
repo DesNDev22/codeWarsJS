07c91fc298a84b53292b3b2dc5af796249fc883d
Luis Carrera <luiscarreradesndev@gmail.com>
2023-05-31 16:15:30 -0500

    my daily katas

--- class29-32c.js
+++ class29-32c.js
@@ -287,4 +287,15 @@ function expressionMatter(a, b, c) {
   console.log('ACSKLDFJSgSKLDFJSKLDFJ'.isUpperCase(), false, 'ACSKLDFJSgSKLDFJSKLDFJ not is upper case');
   console.log('ACSKLDFJSGSKLDFJSKLDFJ'.isUpperCase(), true, 'ACSKLDFJSGSKLDFJSKLDFJ is upper case');
   
+  //
+
+  function findDifference(a, b) {
+    return Math.abs((a.reduce((total, a) => total *= a)) - (b.reduce((total, a) => total *= a)))
+  }
+  
+  console.log(findDifference([3, 2, 5], [1, 4, 4]), 14);
+  console.log(findDifference([9, 7, 2], [5, 2, 2]), 106);
+  console.log(findDifference([11, 2, 5], [1, 10, 8]), 30);
+  console.log(findDifference([4, 4, 7], [3, 9, 3]), 31);
+  console.log(findDifference([15, 20, 25], [10, 30, 25]), 0);
   
