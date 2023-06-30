4a26e7586d3ddac7b858a7a9ece3ac5f68ec8379
Luis Carrera <luiscarreradesndev@gmail.com>
2023-06-30 16:02:10 -0500

    daily challenges friday week 26

--- week26.js
+++ week26.js
@@ -39,4 +39,77 @@ function sumDigits(number) {
 console.log(sumDigits(10), 1);
 console.log(sumDigits(99), 18);
 console.log(sumDigits(-32), 5);
-  
+
+//8kyu get character from ASCII Value
+function getChar(c){
+  return String.fromCharCode(c)
+}
+console.log(getChar(55),'7')
+console.log(getChar(56),'8')
+console.log(getChar(57),'9')
+console.log(getChar(58),':')
+console.log(getChar(59),';')
+console.log(getChar(60),'<')
+console.log(getChar(61),'=')
+console.log(getChar(62),'>')
+console.log(getChar(63),'?')
+console.log(getChar(64),'@')
+console.log(getChar(65),'A')
+
+//8kyu The Wide-Mouthed frog!
+function mouthSize(animal) {
+  return animal.toLowerCase() === 'alligator' ? 'small' : 'wide'
+}
+console.log(mouthSize("toucan"),"wide")
+console.log(mouthSize("ant bear"),"wide")
+console.log(mouthSize("alligator"),"small")
+
+//8kyu 
+function mergeArrays(arr1, arr2) {
+  let uniqueArray = new Set(arr1.concat(arr2))
+  return Array.from(uniqueArray).sort((a, b) => a-b)
+}
+console.log(mergeArrays([], []), [], "Basic tests");
+console.log(mergeArrays([1,2,3,4], [5,6,7,8]), [1,2,3,4,5,6,7,8], "Basic tests");
+console.log(mergeArrays([1,3,5,7,9], [10,8,6,4,2]), [1,2,3,4,5,6,7,8,9,10], "Basic tests");
+console.log(mergeArrays([1,3,5,7,9,11,12], [1,2,3,4,5,10,12]), [1,2,3,4,5,7,9,10,11,12], "Basic tests");
+
+//7kyu Simple Fun #176: Reverse Letter
+function reverseLetter(str) {
+  return str.split('').reverse().filter(a => a.match(/[a-z]/i)).join('')
+}
+console.log(reverseLetter("krishan"),"nahsirk")
+console.log(reverseLetter("ultr53o?n"),"nortlu")
+console.log(reverseLetter("ab23c"),"cba")
+console.log(reverseLetter("krish21an"),"nahsirk")
+
+//8kyu Reverse List Order
+function reverseList(list) {
+  return list.reverse()
+}
+console.log(reverseList([1,2,3,4]), [4,3,2,1]);
+console.log(reverseList([3,1,5,4]), [4,5,1,3]);
+
+//8kyu Grasshopper - Terminal game move function
+function move (position, roll) {
+  return position + roll * 2
+}
+console.log(move(0, 4), 8);
+console.log(move(3, 6), 15);
+console.log(move(2, 5), 12);
+
+//6kyu Count the smiley faces!
+function verifyRules(total, face, index) {
+  return (face.length < 1 || face.length > 3) && (face[0] != ':' && face[0] !=';') || (face[face.length-1] != ')' && face[face.length-1] != 'D') ? total += 0 :
+  face.length === 3 && face[1] != '-' && face[1] != '~' ? total += 0 : total += 1
+}
+
+function countSmileys(arr) {
+  return arr.reduce(verifyRules, 0)
+}
+
+console.log(countSmileys([]                             ), 0);
+console.log(countSmileys(['.D',':~)',';~D',':)']        ), 3);
+console.log(countSmileys([':D',':~)',';~D',':)']        ), 4);
+console.log(countSmileys([':)',':(',':D',':O',':;']     ), 2);
+console.log(countSmileys([';]', ':[', ';*', ':$', ';-D']), 1);
