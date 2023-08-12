6b88ab98a6c51e11d4daaf78cca477150415debc
Luis Carrera <luiscarreradesndev@gmail.com>
2023-08-12 10:01:37 -0500

    push code

--- week31.js
+++ week31.js
@@ -258,3 +258,18 @@ console.log(largestPairSum([10,14,2,23,19]), 42);
 console.log(largestPairSum([-100,-29,-24,-19,19]), 0);
 console.log(largestPairSum([1,2,3,4,6,-1,2]), 10);
 console.log(largestPairSum([-10, -8, -16, -18, -19]), -18);
+
+//8kyu 
+function between(a, b) {
+    const betweenArr = []
+    if (a < b ) {
+    	for (let i = a; i <= b; i++) {
+        let lenArr = 0
+        lenArr = betweenArr.push(i)
+      }
+    }
+ 		return betweenArr
+}
+
+console.log(between(1, 4), [1, 2, 3, 4])
+console.log(between(-2, 2), [-2, -1, 0, 1, 2])
