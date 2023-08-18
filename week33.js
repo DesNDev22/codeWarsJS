a904303b1429d985894162da95da2d0abc867d69
Luis Carrera <luiscarreradesndev@gmail.com>
2023-08-18 17:14:41 -0500

    daily excerises

--- week33.js
+++ week33.js
@@ -34,3 +34,39 @@ const humanYearsCatYearsDogYears = function(humanYears) {
 console.log(humanYearsCatYearsDogYears(1), [1,15,15]);
 console.log(humanYearsCatYearsDogYears(2), [2,24,24]);
 console.log(humanYearsCatYearsDogYears(10), [10,56,64]);
+
+//7kyu Row Weights
+function rowWeights(array){
+    let teamOne = 0
+    let teamtwo = 0
+    const resultArray = [0, 0]
+    
+    for (let i = 0; i < array.length; i++) {
+      i % 2 === 0 ? teamOne += array[i] : teamtwo += array[i]
+    }
+ 		resultArray[0] = teamOne
+  	resultArray[1] = teamtwo
+  	return resultArray
+}
+
+console.log(rowWeights([80]), [80,0]);
+console.log(rowWeights([100,50]), [100,50]);
+console.log(rowWeights([50,60,70,80]), [120,140]);
+console.log(rowWeights([13,27,49]), [62,27]);
+console.log(rowWeights([70,58,75,34,91]), [236,92]);
+console.log(rowWeights([29,83,67,53,19,28,96]), [211,164]);
+console.log(rowWeights([0]), [0,0]);
+console.log(rowWeights([100,51,50,100]), [150,151]);
+console.log(rowWeights([39,84,74,18,59,72,35,61]), [207,235]);
+console.log(rowWeights([0,1,0]), [0,1]);
+
+//8kyu Find the Remainder
+function remainder(n, m){
+    // Divide the larger argument by the smaller argument and return the remainder
+    return Math.min(n,m) != 0 ? Math.max(n,m) % Math.min(n,m) : NaN
+}
+
+console.log(remainder(17,5), 2, 'Returned value should be the value left over after dividing as much as possible. For input n = 17, m = 5');
+console.log(remainder(13, 72), remainder(72, 13), 'The order the arguments are passed should not matter');
+console.log(remainder(1, 0), 'Divide by zero should return NaN');
+console.log(remainder(0, 0), 'Divide by zero should return NaN');
